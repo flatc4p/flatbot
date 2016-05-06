2bbbe15e57d39b0df7e61237b6c37c194aa39b04
Max <max.blank@googlemail.com>
2016-05-06 11:25:54 +0200

    test

--- flatbot.py
+++ flatbot.py
@@ -5,6 +5,7 @@
 
 import socket
 import ssl
+import time
 
 #Settings
 server = "port80a.se.quakenet.org"
@@ -14,21 +15,14 @@ botnick = "flatbot"
 pingflag = 1
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#irc = ssl.wrap_socket(irc_sock)
 
 print "Connecting to %s:%d..." % (server, port)
 #Establishing connection set nickname and join channel
 irc.connect((server, port))
-#irc.send("USER " + botnick + " " + botnick + " " + "RasPi :Testbot\n")
-#irc.send("NICK " + botnick + "\n")
-#irc.send("PRIVMSG nickserv :iNOOP\r\n")
-while pingflag:
-	text = irc.recv(50)
-	if text.find("PING ") != -1:
-		irc.send("PONG " + text.split() [1] + "\r\n")
-		print "PONG " + text.split() [1] + "\r\n"
-		pingflag = 0
-#irc.send("JOIN " + channel)
+irc.send("USER " + botnick + " " + botnick + " " + botnick + ":Testbot\n")
+irc.send("NICK " + botnick + "\n")
+irc.send("PRIVMSG nickserv :iNOOP\r\n")
+irc.send("JOIN " + channel)
 
 while 1:
 	text = irc.recv(2048)
