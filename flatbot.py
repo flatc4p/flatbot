1a915dd954ff21143b3c732da008f03f162d3bdb
Max <max.blank@googlemail.com>
2016-05-06 16:16:35 +0200

    first working release

--- flatbot.py
+++ flatbot.py
@@ -13,20 +13,48 @@ port = 6667
 channel = "#ossiostborn23"
 botnick = "flatbot"
 pingflag = 1
+password = "testpass123"
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print "Connecting to %s:%d..." % (server, port)
 #Establishing connection set nickname and join channel
 irc.connect((server, port))
-irc.send("USER " + botnick + " " + botnick + " " + botnick + ":Testbot\n")
+time.sleep(1)
+#irc.send("PASS " + password)
+#irc.send("USER flatbot . . :Testbot\n")
 irc.send("NICK " + botnick + "\n")
-irc.send("PRIVMSG nickserv :iNOOP\r\n")
-irc.send("JOIN " + channel)
+#irc.send("PRIVMSG nickserv :iNOOP\r\n")
+#irc.send("JOIN " + channel)
 
-while 1:
+while pingflag:	#Answer ping message, register user, join channel
 	text = irc.recv(2048)
 	print text
 	
 	if text.find("PING") != -1:
-		irc.send("PONG " + text.split() [1] + "\r\n")
+		time.sleep(1)
+		answer = "PONG " + text.split("PING :")[1] + "\n"
+		print answer
+		pingflag = 0
+		irc.send(answer)
+		#time.sleep(2)
+		irc.send("USER flatbot . . :A simple Bot\n")
+		time.sleep(4)
+		#irc.send("JOIN " + channel + "\n")
+#irc.send("JOIN " + channel + "\r\n")
+
+while 1:
+	text = irc.recv(2048)
+	print text
+
+	if text.find("PING ") != -1:
+		answer = "PONG " + text.split("PING :")[1] + "\n"
+		print answer
+		irc.send(answer)
+	
+	if text.find("!hi ") != -1:
+		irc.send("PRIVMSG " + channel + " Hello!\n")
+	
+	if text.find("!join") != -1:
+		print "Trying to join channel " + channel + "\r\n"
+		irc.send("JOIN " + channel + "\r\n")
